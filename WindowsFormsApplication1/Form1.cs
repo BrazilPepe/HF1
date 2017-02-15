2ecb341a77661cc4ce08c7c2990d8ea4044ed7b5
balint9610 <balint9610@freemail.hu>
2017-02-15 22:07:27 +0100

    ...

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -10,8 +10,12 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
+
+
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
@@ -95,9 +99,72 @@ namespace WindowsFormsApplication1
             textBox1.Text = ans.ToString();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            
+
+        }
+
+
+        public void számolás()
+        {
+            switch (count)
+            {
+                case 1:
+                    ans = num + float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();                
+                    break;
+
+                case 2:
+                    ans = num - float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+
+                case 3:
+                    ans = num * float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();                
+                    break;
+
+                case 4:
+                    ans = num / float.Parse(textBox1.Text);
+                    textBox1.Text = ans.ToString();
+                    break;
+
+            }
         }
     }
+
+    private void button11_Click(object sender, EventArgs e)
+    {
+        textBox1.Clear();
+        textBox1.Focus();
+        count 1;
+        label1.Text = num.ToString() + "+";
+
+    }
+    private void button12_Click(object sender, EventArgs e)
+    {
+        textBox1.Clear();
+        textBox1.Focus();
+        count 2;
+        label1.Text = num.ToString() + "-";
+
+    }
+    private void button13_Click(object sender, EventArgs e)
+    {
+        textBox1.Clear();
+        textBox1.Focus();
+        count 3;
+        label1.Text = num.ToString() + "*";
+
+    }
+    private void button14_Click(object sender, EventArgs e)
+    {
+        textBox1.Clear();
+        textBox1.Focus();
+        count 4;
+        label1.Text = num.ToString() + "/";
+
+    }
+
+
 }
