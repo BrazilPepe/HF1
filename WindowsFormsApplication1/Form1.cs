3deaee27e94abec65f697385abf463eeccd98189
balint9610 <balint9610@freemail.hu>
2017-02-15 21:04:19 +0100

    Viuális felület, számok textbox-ba kerülése, C, CE feladatra kész.

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,82 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        static double ans;
+        static double num;
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //2-es szám
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //1-es szám
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //3-as szám
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            num = 0;
+            textBox1.Clear();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            ans = 0;
+            num = 0;
+            textBox1.Clear();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ans.ToString();
+        }
     }
 }
